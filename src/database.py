bc085b3796a9cad48e79c5ccb6b1b9c76364b15e
Marilia Fernandez <marilia.fernandez@accenture.com>
2025-07-31 22:06:42 -0300

    document database functions

--- src/database.py
+++ src/database.py
@@ -2,17 +2,32 @@ import mysql.connector
 from dotenv import load_dotenv
 import numpy as np
 import os
+from typing import TypedDict
+
 
 load_dotenv()
 
 
-def parse_fits_data(fits_data: bytes, dtype: str, height: int, width: int):
-    """Parse FITS data from bytes to a numpy array."""
+class FitsFileRecord(TypedDict):
+    fits_data: bytes
+    timestamp: str
+    frame_id: int
+    dtype: str
+    height: int
+    width: int
+
+
+def parse_fits_data(fits_data: bytes, dtype: str, height: int, width: int) -> np.ndarray:
+    """
+    Parse FITS data from bytes to a numpy array
+    """
     return np.frombuffer(fits_data, dtype=dtype).reshape((height, width))
 
 
 def connect_to_database():
-    # Connect to server
+    """
+    Connect to MySQL database using environment variables
+    """
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
         port=os.getenv("DB_PORT", 3306),
@@ -30,17 +45,16 @@ def test_connection():
     else:
         cur = conn.cursor()
         cur.execute("SELECT CURDATE()")
-        # Fetch one result
         row = cur.fetchone()
         print("Current date is: {0}".format(row[0]))
-        # Close cursor
         cur.close()
-        # Close connection
         conn.close()
 
 
 def load_fits_records():
-    """Load FITS file metadata from the database."""
+    """
+    Load FITS file metadata from the database
+    """
     conn = connect_to_database()
     cursor = conn.cursor(dictionary=True)
 
@@ -50,19 +64,24 @@ def load_fits_records():
     cursor.close()
     conn.close()
 
-    return [ record |
-       {"array": parse_fits_data(
-            record["fits_data"],
-            dtype=record["dtype"],
-            height=record["height"],
-            width=record["width"],
-        )}
+    return [
+        record
+        | {
+            "array": parse_fits_data(
+                record["fits_data"],
+                dtype=record["dtype"],
+                height=record["height"],
+                width=record["width"],
+            )
+        }
         for record in results
     ]
 
 
-def insert_fits_data(data: dict):
-    """Insert FITS file and metadata into the database."""
+def insert_fits_data(data: FitsFileRecord):
+    """
+    Insert FITS file and metadata into the database
+    """
     conn = connect_to_database()
     cursor = conn.cursor()
 
@@ -70,12 +89,8 @@ def insert_fits_data(data: dict):
     columns = ", ".join(data.keys())
     sql = f"INSERT INTO fits_files ({columns}) VALUES ({placeholders})"
 
-    print(f"Executing SQL: {sql}")
-
     cursor.execute(sql, list(data.values()))
     conn.commit()
 
     cursor.close()
     conn.close()
-
-
